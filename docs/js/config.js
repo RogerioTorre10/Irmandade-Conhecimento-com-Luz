818b2dc9ecfbc64b6dcb4091481d5ebc79b62d2d
RogerioTorre10 <superarsempre@gmail.com>
2025-08-20 21:13:57 -0300

    Update config.js

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,8 +1,9 @@
-// Configurações centrais da Jornada
+// Configurações globais da Jornada
 window.JORNADA_CFG = {
   API_BASE: 'https://conhecimento-com-luz-api.onrender.com',
   PDF_ENDPOINT: '/gerar-pdf',
   HQ_ENDPOINT: '/gerar-hq',
-  STORAGE_KEY: 'jornadaEssencial_v1',
-};
 
+  // 🔐 Senha fixa de teste (minúsculas): "iniciar"
+  PASSWORD: 'iniciar'
+};
