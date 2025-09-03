f793ae49951400d2437ba1275875ef0a3113e5ed
RogerioTorre10 <superarsempre@gmail.com>
2025-09-03 00:22:57 -0300

    Update jornada-controller.js

--- public/jornada-controller.js
+++ public/jornada-controller.js
@@ -110,6 +110,15 @@ const analiseSentimento = (texto) => {
 // CONTROLE DE PROGRESSO POR BLOCOS E PERGUNTAS
 // ================================================================
   window.JORNADA_ENTRAR_BLOCO = (i, qtdPerguntas) => {
+     J.blocoAtual = i;
+     J.perguntasNoBloco = qtdPerguntas;
+     J.idxPerguntaNoBloco = 0;
+    
+    // NADA DEVE SER MOSTRADO AQUI.
+    // Apenas atualiza as informações do bloco.
+    JORNADA_UI.setProgressoBlocos(i, J.totalBlocos);
+    JORNADA_UI.setProgressoPerguntas(0);
+};
      // Mostra a pergunta atual
     const atual = perguntas[state.perguntaIndex];
     U.show(atual);
