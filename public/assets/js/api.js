ab409235112ca93bb058fc3526c44da606fe040a
RogerioTorre10 <superarsempre@gmail.com>
2025-09-26 11:46:55 -0300

    Update api.js

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -84,14 +84,5 @@
 
   console.log('[API] pronto · PRIMARY=', API_PRIMARY, '· FALLBACK=', API_FALLBACK);
 })();
-(function () {
-  const API = {
-    init() {
-      console.log('[API] pronto');
-    },
-  };
 
-  window.API = API;
-  window.__markReady && window.__markReady('API');
-})();
 
