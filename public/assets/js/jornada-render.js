de68e2602e8d7ccc697e267eeb6d4b6660919fdc
RogerioTorre10 <superarsempre@gmail.com>
2025-09-28 18:50:06 -0300

    Update jornada-render.js

--- public/assets/js/jornada-render.js
+++ public/assets/js/jornada-render.js
@@ -1,11 +1,5 @@
 (function () {
   'use strict';
-import i18n from '/public/assets/js/i18n.js';
-
-const paperLog = (...args) => console.log('[JORNADA_RENDER]', ...args);
-
-async
-
   
   const JR = (window.JORNADA_RENDER = window.JORNADA_RENDER || {});
 
