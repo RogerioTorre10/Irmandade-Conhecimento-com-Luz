3c3ed4462d1d62d3f0aeb898aad05ec4b4e0a9c2
RogerioTorre10 <superarsempre@gmail.com>
2025-10-05 12:29:31 -0300

    Update jornada-bootstrap.js

--- public/assets/js/jornada-bootstrap.js
+++ public/assets/js/jornada-bootstrap.js
@@ -1,12 +1,3 @@
-<script>
-(function (global) {
-  'use strict';
-
-  if (global.__JornadaBootstrapReady) {
-    console.log('[BOOT] Já carregado, ignorando');
-    return;
-  }
-  global.__JornadaBootstrapRead<script>
 (function (global) {
   'use strict';
 
@@ -43,29 +34,3 @@
     setTimeout(fire, 0);
   }
 })(window);
-</script>y = true;
-
-  console.log('[BOOT] Iniciando micro-boot…');
-
-  async function fire() {
-    try {
-      if (global.i18n) {
-        await global.i18n.waitForReady(5000);
-        console.log('[BOOT] i18n pronto, disparando bootstrapComplete');
-      } else {
-        console.warn('[BOOT] i18n não encontrado, prosseguindo sem esperar');
-      }
-      document.dispatchEvent(new CustomEvent('bootstrapComplete'));
-      console.log('[BOOT] Evento bootstrapComplete disparado');
-    } catch (e) {
-      console.error('[BOOT] Erro ao disparar bootstrapComplete:', e);
-    }
-  }
-
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', fire, { once: true });
-  } else {
-    setTimeout(fire, 0);
-  }
-})(window);
-</script>
