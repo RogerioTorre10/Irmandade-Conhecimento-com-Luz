666f7f82c439831a371899eed8818b1968a2b676
RogerioTorre10 <superarsempre@gmail.com>
2025-10-09 19:30:42 -0300

    Create section-senha.js

---
+++ public/assets/js/section-senha.js
@@ -0,0 +1,5 @@
+(function () {
+    'use strict';
+    // A lógica desta seção é gerenciada no jornada-secoes.js e controller.js
+    console.log('[Senha] Lógica transferida para o Controller/Secoes.');
+})();
