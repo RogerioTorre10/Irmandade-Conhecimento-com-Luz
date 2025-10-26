114448681e475e93c13097631d6e5afe2f3e9ee8
RogerioTorre10 <superarsempre@gmail.com>
2025-10-26 11:28:35 -0300

    Update section-perguntas.js

--- public/assets/js/section-perguntas.js
+++ public/assets/js/section-perguntas.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
     // Este arquivo conterá a lógica específica de perguntas, se for necessária,
-    // mas a maior parte é gerenciada pelo jornada-secoes.js
+    // mas a maior parte é gerenciada pelo jornada-paper-qa.js e o jornada-secoes.js
     console.log('[Perguntas] Script carregado. A lógica é centralizada no JSecoes.');
 })();
