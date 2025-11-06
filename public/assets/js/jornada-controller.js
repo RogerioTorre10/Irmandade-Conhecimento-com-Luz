a90ea3de1f9932892aab34579dd91a07f2f6c913
RogerioTorre10 <superarsempre@gmail.com>
2025-11-05 21:26:55 -0300

    Update jornada-controller.js

--- public/assets/js/jornada-controller.js
+++ public/assets/js/jornada-controller.js
@@ -248,20 +248,7 @@
       attachButtonEvents(sectionId, node);
       handleSectionLogic(sectionId, node);
     }
-  });
-  // LIMPA DADOS ANTIGOS AO INICIAR NOVA JORNADA
-     window.addEventListener('load', () => {
-     const LIMPAR_CHAVES = [
-    'jornada.nome', 'jornada.guia', 'jornada.participante',
-    'jornada.selfieDataUrl', 'selfie.dataUrl', 'selfieImageData',
-    'jc.selfie', 'user.name'
-    ];
-     LIMPAR_CHAVES.forEach(key => {
-     sessionStorage.removeItem(key);
-     localStorage.removeItem(key);
-    });
-     console.log('[INIT] Dados antigos limpos para nova jornada');
-  });
+  });  
 
     init();
   })();
