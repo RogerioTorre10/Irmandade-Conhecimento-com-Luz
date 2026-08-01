5ba1f691583fe9b23dd37e228970a003edc6030b
RogerioTorre10 <superarsempre@gmail.com>
2026-08-01 00:24:37 -0300

    Update section-senha.js

--- public/assets/js/section-senha.js
+++ public/assets/js/section-senha.js
@@ -394,7 +394,7 @@
       || '/assets/videos/filme-senha-confirmada.mp4';
   }
 
-  function saveSenha(value) {
+  async function initOnce(root, myToken) {
     try {
       if (window.JC?.data) {
         window.JC.data.senha = value;
