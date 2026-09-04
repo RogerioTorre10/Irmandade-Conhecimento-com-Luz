2511e7aacd463a9c1ed6ee912ab68b6b92034bc4
RogerioTorre10 <superarsempre@gmail.com>
2026-09-04 12:13:37 -0300

    Update section-senha.js

--- public/assets/js/section-senha.js
+++ public/assets/js/section-senha.js
@@ -681,11 +681,7 @@
         'jornada_auth_ok',
         '1'
       );
-
-      if (window.JORNADA_SESSION) {
-        window.JORNADA_SESSION.reauthRequired = false;
-      }
-
+      
       if (startedAt) {
         localStorage.setItem(
           'jornada_started_at',
